63ae10089ad869a42339a03599fd21770a5552ee
SamHarris2020 <sam.career@outlook.com>
2018-11-22 00:30:17 -0700

    Saved Token From Pirates

--- src/com/company/Connection.java
+++ src/com/company/Connection.java
@@ -9,7 +9,7 @@ public class Connection {
         JDA discord = null;
 
         try {
-            discord = new JDABuilder(AccountType.BOT).setToken("NDgxOTA2ODg5NjA5OTY5NjY2.Dl9lGg.CVdiSzlB2_vUMoZgOdWdBvoLpzo").buildBlocking();
+            discord = new JDABuilder(AccountType.BOT).setToken("Nope").buildBlocking();
         } catch (LoginException | InterruptedException e) {
             e.printStackTrace();
         }
